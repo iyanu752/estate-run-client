8d70938f5b633da5a79f3ebad0c354f43be03b83
Ahmed iyanuoluwa <127423641+iyanu752@users.noreply.github.com>
2025-08-09 13:10:35 +0100

    removed unused values

--- src/page/checkout/checkoutPage.tsx
+++ src/page/checkout/checkoutPage.tsx
@@ -65,8 +65,8 @@ export default function CheckoutPage() {
   });
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
   const [ispacked, setIspacked] = useState(false);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [supermarket, setSupermarket] = useState<Supermarket[]>([]);
+   
+  const [, setSupermarket] = useState<Supermarket[]>([]);
   const subtotal = cartItems.reduce(
     (sum, item) => sum + item.price * item.quantity,
     0
